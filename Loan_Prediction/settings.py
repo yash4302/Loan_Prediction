b44b966ba1dd4a5437dbcec7804c515f213c7c29
Tanmay Damle <tanmay.damle.25@gmail.com>
2021-11-11 18:45:49 +0530

    Hosting Changes -3

--- Loan_Prediction/settings.py
+++ Loan_Prediction/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Loan_Prediction.urls'
@@ -131,3 +132,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
